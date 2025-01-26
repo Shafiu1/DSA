2e025aa3067e0890600b9db885f4024c2c267b9d
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-26 15:42:07 +0600

    singly linked list

---
+++ Linked_List/singlyLinkedList.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void InsertAtHead(Node* &head, int data){
+    Node* temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+
+void InsertAtTail(Node* &head,Node* &tail ,int data){
+    Node* temp = new Node(data);
+    Node* last=head;
+    if(head==NULL){
+        temp->next=head;
+        head=temp;
+    }else{
+        while(last->next!=NULL){
+            last=last->next;
+        }
+        last->next=temp;
+        tail=temp;
+    }
+}
+
+void ListSize(Node* head){
+    int counter =0;
+    while(head!=NULL){
+        counter+=1;
+        head=head->next;
+    }
+    cout<<counter<<endl;
+}
+
+void InsertAtTailUsingTailPointer(Node* &tail, int data)
+{
+    Node *temp = new Node(data);
+    tail->next=temp;
+    tail=tail->next;
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node* node1=new Node(33);
+    Node* head = node1;
+    Node* tail = node1;
+    InsertAtHead(head,20);
+    InsertAtHead(head,43);
+    InsertAtHead(head,43);
+    InsertAtHead(head,23);
+    InsertAtHead(head,240);
+    InsertAtTail(head,tail,55);
+    InsertAtTailUsingTailPointer(tail,65);//55 is not added as the tail is not updated in the previous line
+    print(head);
+    ListSize(head);
+    return 0;
+}
