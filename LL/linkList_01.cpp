00f878122e04d38cd06f2b53b8ba7167bdaa71c5
Shafiul Alam <shafiulalam0307@gmail.com>
2025-08-29 19:35:55 +0600

    linked list first commit

---
+++ LL/linkList_01.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int val){
+        data = val;
+        next=NULL;
+    }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+public:
+    List(){
+        head=tail=NULL;
+    }
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+            return;
+        }else{
+            newNode->next=head;
+            head=newNode;
+        }
+    }
+    void printLL(){
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+    void push_back(int val){
+        Node* newNode=new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+        }else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+
+    void pop_front(){
+        Node* temp=head;
+        if(head==NULL){
+            cout<<"The list is empty";
+            return;
+        }
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+};
+
+int main(){
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+    ll.push_back(0);
+    ll.pop_front();
+    ll.printLL();
+}
